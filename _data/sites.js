a640a771e14ab2018c7ad68e80d6daa9eb8fd10d
huzic <pavel.huza@gmail.com>
2020-07-03 13:16:40 +0200

    Update sites.js

--- _data/sites.js
+++ _data/sites.js
@@ -1,56 +1,43 @@
 module.exports = {
-	netlify: {
-		name: "Netlify", // optional, falls back to object key
-		description: "Netlify web sites",
+	danfil: {
+		name: "Danfil", // optional, falls back to object key
+		description: "Danfil.cz web site",
 		options: {
 			frequency: 60 * 24, // 24 hours
 			freshChrome: "site",
 		},
 		urls: [
-			"https://www.netlify.com/",
-			"https://www.netlify.com/donation-matching/",
-			"https://www.netlify.com/products/build/plugins/",
-			"https://www.netlify.com/with/sitecore/",
-			"https://www.netlify.com/with/drupal/",
-			"https://www.netlify.com/with/wordpress/",
-			"https://www.netlify.com/webinar/a-drupal-journey-to-the-jamstack/",
-			"https://jamstackconf.com/",
-			"https://jamstackconf.com/virtual/",
+			"https://www.danfil.cz/",
+			"https://www.danfil.cz/nausnice/dite/,
+			"https://www.danfil.cz/detske-nausnicky-danfil-srdicka-c2160-ze-zluteho-zlata-s-kaminky-white-zapinani-brizura-11147/",
+			"https://www.danfil.cz/konfigurator/",
+			"https://www.danfil.cz/konfigurator/detske-nausnicky-danfil-kyticky-c1945-ze-zluteho-zlata-s-kaminkem-white-zapinani-brizura-3621/",
 		]
 	},
-	ssg: {
-		name: "SSG", // optional, falls back to object key
-		description: "Static Site Generator web sites",
-		options: {
-			frequency: 60 * 24, // 24 hours
-			// if the sites don’t share assets on the same domain, we can reset
-			// chrome with each run instead of each site in every run (it’s faster)
-			freshChrome: "run"
-		},
-		urls: [
-			"https://www.11ty.dev/",
-			"https://www.gatsbyjs.org/",
-			"https://gohugo.io/",
-			"https://nextjs.org/",
-			"https://nuxtjs.org/",
-			"https://gridsome.org/",
-		]
-	},
-	zachleat: {
-		name: "zachleat.com", // optional, falls back to object key
-		description: "Personal web site",
+	brilianty: {
+		name: "Brilianty", // optional, falls back to object key
+		description: "Brilianty.cz web site",
 		options: {
 			frequency: 60 * 24, // 24 hours
 			freshChrome: "site"
 		},
 		urls: [
-			"https://www.zachleat.com/",
-			"https://www.zachleat.com/web/",
-			"https://www.zachleat.com/web/about/",
-			"https://www.zachleat.com/twitter/",
-			"https://www.zachleat.com/resume/",
-			"https://www.zachleat.com/web/comprehensive-webfonts/",
-			"https://www.zachleat.com/web/google-fonts-display/",
+			"https://www.brilianty.cz/",
+			"https://www.brilianty.cz/prsteny/",
+			"https://www.brilianty.cz/zlaty-zasnubni-prsten-df-2947-bile-zlato-s-briliantem-9909/",
+		]
+	},
+	dfprsteny: {
+		name: "DFprsteny", // optional, falls back to object key
+		description: "Dfprsteny.cz web site",
+		options: {
+			frequency: 60 * 24, // 24 hours
+			freshChrome: "site"
+		},
+		urls: [
+			"https://www.dfprsteny.cz/",
+			"https://www.dfprsteny.cz/cz/kolekce/basic/",
+			"https://www.dfprsteny.cz/cz/snubni-prsteny-ze-zluteho-zlata-1-7-mm-pulkulaty-par-4i17-p115/",
 		]
 	}
 };
